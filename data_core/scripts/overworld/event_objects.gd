9257d2be15e76ba21ad9379d639642aec886770d
AxelLoquendo <sagitarioloqd@gmail.com>
2026-07-19 18:50:30 -0500

    Agregando más skins para player

--- data_core/scripts/overworld/event_objects.gd
+++ data_core/scripts/overworld/event_objects.gd
@@ -18,7 +18,8 @@ enum PlayerID {
 	OBJ_EVENT_GFX_LEAF_FRLG,
 	# Jotho
 	OBJ_EVENT_GFX_ECO_HGSS,
-	OBJ_EVENT_GFX_CRISTI_HGSS,
+	OBJ_EVENT_GFX_CRISTI_GPC,
+	OBJ_EVENT_GFX_LYRA_HGSS,
 	# Hoenn
 	OBJ_EVENT_GFX_BRUNO_RSB,
 	OBJ_EVENT_GFX_AURA_RSB,
@@ -44,16 +45,19 @@ const player_sprites: Array[String] = [
 	".",
 	"res://game/graphics_eb/overworld/player/male/kael/normal.png",
 	"res://game/graphics_eb/overworld/player/female/kaida/normal.png",
-	".",
+	"res://graphics/overworld/player/male/red/normal.png",
 	"res://graphics/overworld/player/female/leaf/normal.png",
 	"res://graphics/overworld/player/male/eco/normal.png",
-	".",
+	"res://graphics/overworld/player/female/cristi/normal.png",
+	"res://graphics/overworld/player/female/lyra/normal.png",
 	"res://graphics/overworld/player/male/bruno/normal.png",
 	"res://graphics/overworld/player/female/aura/normal.png",
-	".",
-	".",
+	"res://graphics/overworld/player/male/leon/normal.png",
+	"res://graphics/overworld/player/female/maya/normal.png",
 	"res://graphics/overworld/player/male/lucho/normal.png",
 	"res://graphics/overworld/player/female/liza/normal.png",
+	"res://graphics/overworld/player/male/risso/normal.png",
+	"res://graphics/overworld/player/female/nanci/normal.png",
 ]
 
 static var casillas_ocupadas: Dictionary = {}
