5266f44b984e24f464934e81720aa7eed4333da3
Axel_CodeInfinity <sagitarioloqd@gmail.com>
2026-08-24 19:42:12 -0500

    fix: Update for Godot 4.7 API (add_control_to_bottom_panel, remove EditorUndoRedo)

--- addons/species_editor/plugin.gd
+++ addons/species_editor/plugin.gd
@@ -4,17 +4,15 @@ extends EditorPlugin
 class_name SpeciesEditorPlugin
 
 var species_dock: SpeciesDock
-var undo_redo: EditorUndoRedo
 
 func _enter_tree() -> void:
-	undo_redo = get_undo_redo()
 	species_dock = SpeciesDock.new()
-	# Posicionar en el panel inferior (como Output, Debugger, etc)
-	add_control_to_dock(DOCK_SLOT_BOTTOM_BR, species_dock)
+	# Godot 4.7: DOCK_SLOT_BOTTOM_BR no existe, usamos add_control_to_bottom_panel
+	add_control_to_bottom_panel(species_dock, "🔬 Species Editor")
 	print("✓ Species Editor Plugin loaded")
 
 func _exit_tree() -> void:
 	if species_dock:
-		remove_control_from_docks(species_dock)
+		remove_control_from_bottom_panel(species_dock)
 		species_dock.queue_free()
 	print("✓ Species Editor Plugin unloaded")
