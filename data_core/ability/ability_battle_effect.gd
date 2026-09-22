3f0df81f40ae3a53eeede28738a625aef72e6f84
AxelLoquendo <sagitarioloqd@gmail.com>
2026-09-22 11:26:26 -0500

    Preparación Terrain Surges

--- data_core/ability/ability_battle_effect.gd
+++ data_core/ability/ability_battle_effect.gd
@@ -17,3 +17,11 @@ enum terrainID{
 	TERRAIN_MISTY,
 	TERRAIN_PSYCHIC
 }
+
+const BG_TERRAIN_SPRITES: Dictionary = {
+	terrainID.TERRAIN_NONE: "",
+	terrainID.TERRAIN_ELECTRIC: "res://graphics/battle_ground/BG_Electric_Surge.png",
+	terrainID.TERRAIN_GRASSY: "res://graphics/battle_ground/BG_Grassy_Surge.png",
+	terrainID.TERRAIN_MISTY: "res://graphics/battle_ground/BG_Misty_Surge.png",
+	terrainID.TERRAIN_PSYCHIC: "res://graphics/battle_ground/BG_Psychic_Surge.png"
+}
